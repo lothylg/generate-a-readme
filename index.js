6c0fbb56150ba1647bc110742eff7973eb9f191a
lothylg <lothygresser@gmail.com>
2024-07-29 15:32:09 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const inquirer = require("inquirer");
+const {writeFile} = require('fs');
+const {generateReadme} = require("./readme");
+const {getQuestions} = require("./questions");
+const questions = getQuestions();
+const {generateBadge} = require("./badgelogic");
+// const badges = generateBadge();
+
+async function start(){
+    const responses = await inquirer.prompt(questions);
+    const readme = generateReadme(responses);
+
+  
+    writeFile("README.md", `${readme}`, (err) => {
+      if( err ){
+        console.error("There was an error writing the file.")
+      } else {
+        console.log("File written successfully!")
+      }
+    })
+  }
+  start();
